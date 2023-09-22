374670453a56b6f1c709c268b3055accaef3efc0
mitanshi25 <mitanshi25ksj@gmail.com>
2023-09-22 03:29:00 -0400

    added a api for adding the guest

--- hotelproject/hotelapp/views.py
+++ hotelproject/hotelapp/views.py
@@ -13,3 +13,30 @@ def showallguest (request):
 
     }
     return render(request, 'showallguest.html', context)
+
+def addAGuest (request):
+
+    if request.method == 'POST':
+
+        
+
+        guest = Guest()
+        guest.first_name = request.POST['fname']
+        guest.last_name =request.POST['lname']
+        guest.gov_id =request.POST['gov-id']
+        guest.check_in_time =request.POST['checkin']
+        guest.check_out =request.POST['checkout']
+        current_room = request.POST['roomno']
+        guest.room_number = Rooms.objects.get(room_no = current_room)
+        guest.save()
+        
+        # return render (request, 'addGuest.html')
+    
+
+
+    guest = Guest.objects.all()
+    context = {
+        'guests' : guest
+
+    }
+    return render(request, 'addGuest.html', context)
